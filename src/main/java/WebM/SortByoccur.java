4dd2547730719f7f79af4a9cc2f86f1ff2828e87
vibhavsingh <40350892+vibhavsingh@users.noreply.github.com>
2018-07-05 14:19:06 +0530

    CheckZone

---
+++ src/main/java/WebM/SortByoccur.java
@@ -0,0 +1,13 @@
+package WebM;
+
+import java.util.Scanner;
+
+public class SortByoccur {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		
+	}
+
+}
