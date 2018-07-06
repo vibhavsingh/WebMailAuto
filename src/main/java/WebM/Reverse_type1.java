2ad69a3c4c90ff98857dfff34524e9f739a9ff22
vibhavsingh <40350892+vibhavsingh@users.noreply.github.com>
2018-07-06 18:54:46 +0530

    Reverse by wrd-TYPE I

--- src/main/java/WebM/Reverse_type1.java
+++ src/main/java/WebM/Reverse_type1.java
@@ -1,17 +1,33 @@
 package WebM;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reverse_type1 {
 
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		s=s+" ";
-		for(int i=0;i<s.length();i++)
-		{
-			
+	public static void main(String[] args) {
+		String k = null;
+		String s = "Your software testing partner";
+		s = " " + s + " ";
+		int len = s.length();
+		int p = 0;
+		int le = 1;
+		ArrayList<Integer> arrli = new ArrayList<Integer>(le);
+		char[] ch = s.toCharArray();
+		for (int i = 0; i < s.length(); i++) {
+			if (ch[i] == ' ') {
+				arrli.add(i);
+
+			}
+
 		}
+		// System.out.println(arrli);
+		// String str=
+		for (int j = arrli.size() - 2; j >= 0; j--) {
+			String str = s.substring(arrli.get(j), arrli.get(j + 1));
+			System.out.print(str);
+		}
+
 	}
+
 }
